b7fad0f1f8a409641d323cd9e5ea92679cb2aea0
maisvendoo <maisvendoo@gmail.com>
2015-04-16 12:51:50 +0400

    Framework detection code

--- dotnet.pas
+++ dotnet.pas
@@ -1,52 +1,90 @@
-[Code]
-
 //-----------------------------------------------------------------------------
 //  Проверка наличия нужного фреймворка
 //-----------------------------------------------------------------------------
-function IsDotNetDetected(version: string; service: cardinal): boolean;
+function IsDotNetDetected(version: string; release: cardinal): boolean;
 
-var
-
-  reqNetVer: string;
-  check45: boolean;
-  reg_key: string;
-  success: boolean;
-  install: cardinal;
-  serviceCount: cardinal;
-  release: cardinal;
+var 
+    reg_key: string; // Просматриваемый подраздел системного реестра
+    success: boolean; // Флаг наличия запрашиваемой версии .NET
+    release45: cardinal; // Номер релиза для версии 4.5.x
+    key_value: cardinal; // Прочитанное из реестра значение ключа
+    sub_key: string;
 
 begin
 
-  if version = 'v4.5' then
-    begin
-        version := 'v4\Full';
-        check45 := true;
-    end
-  else
-        check45 := false;
+    success := false;
+    reg_key := 'SOFTWARE\Microsoft\NET Framework Setup\NDP\';
+    
+    // Вресия 3.0
+    if Pos('v3.0', version) = 1 then
+      begin
+          sub_key := 'v3.0';
+          reg_key := reg_key + sub_key;
+          success := RegQueryDWordValue(HKLM, reg_key, 'InstallSuccess', key_value);
+          success := success and (key_value = 1);
+      end;
 
-  reg_key := 'SOFTWARE\Microsoft\NET Framework Setup\NDP\' + version;
+    // Вресия 3.5
+    if Pos('v3.5', version) = 1 then
+      begin
+          sub_key := 'v3.5';
+          reg_key := reg_key + sub_key;
+          success := RegQueryDWordValue(HKLM, reg_key, 'Install', key_value);
+          success := success and (key_value = 1);
+      end;
 
-  if Pos('v3.0', version) = 1 then
-    begin
-        success := success and RegQueryDWordValue(HKLM, reg_key + '\Setup', 'InstallSuccess', install);
-    end
-  else
-    begin
-        success := RegQueryDWordValue(HKLM, reg_key, 'Install', install);
-    end;
+     // Вресия 4.0 клиентский профиль
+     if Pos('v4.0 Client Profile', version) = 1 then
+      begin
+          sub_key := 'v4\Client';
+          reg_key := reg_key + sub_key;
+          success := RegQueryDWordValue(HKLM, reg_key, 'Install', key_value);
+          success := success and (key_value = 1);
+      end;
 
-  if Pos('v4', version) = 1 then
-    success := success and RegQueryDWordValue(HKLM, reg_key, 'Servicing', serviceCount)
-  else
-    success := success and RegQueryDWordValue(HKLM, reg_key, 'SP', serviceCount);
+     // Вресия 4.0 расширенный профиль
+     if Pos('v4.0 Full Profile', version) = 1 then
+      begin
+          sub_key := 'v4\Full';
+          reg_key := reg_key + sub_key;
+          success := RegQueryDWordValue(HKLM, reg_key, 'Install', key_value);
+          success := success and (key_value = 1);
+      end;
 
-  if check45 then
-    begin
-        success := success and RegQueryDWordValue(HKLM, reg_key, 'Release', release);
-        success := success and (release >= 378389);
-    end;
+     // Вресия 4.5
+     if Pos('v4.5', version) = 1 then
+      begin
+          sub_key := 'v4\Full';
+          reg_key := reg_key + sub_key;
+          success := RegQueryDWordValue(HKLM, reg_key, 'Release', release45);
+          success := success and (release45 >= release);
+      end;
+        
+    result := success;
 
-  result := success and (install = 1) and (serviceCount >= service);
 end;
 
+//-----------------------------------------------------------------------------
+//  Функция-обертка для детектирования конкретной нужной нам версии
+//-----------------------------------------------------------------------------
+function IsRequiredDotNetDetected(): boolean;
+begin
+    result := IsDotNetDetected('v4.0 Full Profile', 0);
+end;
+
+//-----------------------------------------------------------------------------
+//    Callback-функция, вызываемая при инициализации установки
+//-----------------------------------------------------------------------------
+function InitializeSetup(): boolean;
+begin
+
+  // Если нет тербуемой версии .NET выводим сообщение о том, что инсталлятор
+  // попытается установить её на данный компьютер
+  if not IsDotNetDetected('v4.0 Full Profile', 0) then
+    begin
+      MsgBox('{#Name} requires Microsoft .NET Framework 4.0 Full Profile.'#13#13
+             'The installer will attempt to install it', mbInformation, MB_OK);
+    end;   
+
+  result := true;
+end;
